23fbafd5fda22c1a3dc70c785569869c529903ad
mdamian <mdamian@homeaway.com>
2020-06-17 15:38:36 +0200

    Solved issue with LocalDateTime properly

---
+++ ddls/sensor_data.sql
@@ -0,0 +1,7 @@
+CREATE TABLE sensor_data (
+    sensor              varchar(50),
+    airHumidity         numeric(10,5),
+    airTemperature      numeric(10,5),
+    soilHumidity        numeric(10,5),
+    collectionDateTime  timestamp
+);
--- src/main/scala/com/falcon/garden/db/SensorRepository.scala
+++ src/main/scala/com/falcon/garden/db/SensorRepository.scala
@@ -1,28 +1,21 @@
 package com.falcon.garden.db
 
-import java.time.LocalDateTime
-
 import cats.effect.IO
 import com.falcon.garden.domain.SensorData
+import doobie.implicits._
 import doobie.util.transactor.Transactor
-import cats._, cats.data._, cats.implicits._
-import doobie._, doobie.implicits._
 
 class SensorRepository(xa: Transactor[IO]) {
 
-  implicit val ldtPut: Put[LocalDateTime] = Put[LocalDateTime]
-  implicit val ldtGet: Get[LocalDateTime] = Get[LocalDateTime]
-
-  def save(s: SensorData): IO[Unit] = {
+  def save(s: SensorData): IO[Unit] =
     sql"""INSERT INTO sensor_data (sensor, airHumidity, airTemperature, soilHumidity, collectionDateTime)
           VALUES(${s.sensor}, ${s.airHumidity}, ${s.airTemperature}, ${s.soilHumidity}, ${s.collectionDateTime})
          """.update.run
       .transact(xa)
       .map(_ -> ())
-  }
 
   def retrieveLatest(limit: Int): IO[List[SensorData]] =
-    sql"""select * from sensor_data limit $limit"""
+    sql"""select * from sensor_data order by collectionDateTime desc limit $limit"""
       .query[SensorData]
       .to[List]
       .transact(xa)
